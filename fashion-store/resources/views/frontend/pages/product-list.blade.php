d3564931e3e13dd43b6d02db8a83ad5632403aac
karthihub <karthiikeyansekar@gmail.com>
2024-05-04 17:56:06 +0530

    Product List Page and Product Details Page UI Fixes

--- fashion-store/resources/views/frontend/pages/product-list.blade.php
+++ fashion-store/resources/views/frontend/pages/product-list.blade.php
@@ -21,7 +21,7 @@
 
 /* Remove default underline from links */
 a {
-  text-decoration: none;
+    text-decoration: none;
 }
 
 /* Remove default list-style from lists */
@@ -33,6 +33,13 @@ li {
 h1 {
   font-weight: 600;
   font-size: 1.5rem;
+}
+
+.overflow-hidden{
+    width: 100px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
 }
 
 /* Set font family for the body */
@@ -74,6 +81,8 @@ h1 {
   background-color: #fff;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   padding: 20px 20px;
+  border: 1px solid #DA627D;
+  border-radius: 0.5rem;
 }
 #side-color{
     background-color: #fff;
@@ -186,7 +195,166 @@ a.sidebar-link:hover {
     }
 }
 
+.sidebar_widget .single_banner {
+  border: 0;
+}
+@media only screen and (max-width: 767px) {
+  .sidebar_widget .single_banner {
+    margin-bottom: 0;
+  }
+}
+@media only screen and (max-width: 767px) {
+  .sidebar_widget .single_banner a {
+    width: 100%;
+  }
+}
+@media only screen and (max-width: 767px) {
+  .sidebar_widget .single_banner a img {
+    width: 100%;
+  }
+}
 
+.widget_list {
+  margin-bottom: 29px;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 35px;
+}
+.widget_list:last-child {
+  /* margin-bottom: 0;
+  padding-bottom: 0;
+  border-bottom: 0; */
+}
+.widget_list h2 {
+  font-size: 16px;
+  margin: 0 0 15px;
+  padding: 0 0 5px;
+  text-transform: capitalize;
+  font-weight: 700;
+  line-height: 24px;
+}
+.widget_list > ul > li {
+  margin-bottom: 8px;
+  position: relative;
+}
+.widget_list > ul > li:last-child {
+  margin-bottom: 0;
+}
+.widget_list > ul > li input {
+  position: absolute;
+  left: 3px;
+  top: 50%;
+  transform: translatey(-50%);
+  opacity: 0;
+  cursor: pointer;
+  z-index: 999;
+}
+.widget_list > ul > li input:checked ~ .checkmark {
+  background-color: #DA627D;
+  border: 1px solid #DA627D;
+}
+.widget_list > ul > li input:checked ~ .checkmark::before {
+  display: block;
+}
+.widget_list > ul > li > a {
+  font-size: 14px;
+  display: block;
+  line-height: 27px;
+  margin-left: 30px;
+  text-transform: capitalize;
+  font-family: "poppins";
+}
+.widget_list > ul > li > a:hover {
+  color: #DA627D;
+}
+.widget_list > ul > li span.checkmark {
+  height: 17px;
+  width: 17px;
+  border: 1px solid #dfdfdf;
+  border-radius: 3px;
+  display: block;
+  position: absolute;
+  top: 50%;
+  transform: translatey(-50%);
+  background: #f4f4f4;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+}
+.widget_list > ul > li span.checkmark::before {
+  left: 5px;
+  top: 3px;
+  width: 5px;
+  height: 8px;
+  border: solid white;
+  border-top-width: medium;
+  border-right-width: medium;
+  border-bottom-width: medium;
+  border-left-width: medium;
+  border-width: 0 2px 2px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.widget_list.widget_filter form {
+  padding-top: 10px;
+}
+.widget_list.widget_filter form input {
+  background: none;
+  border: none;
+  font-size: 12px;
+  float: right;
+  text-align: right;
+  line-height: 31px;
+}
+@media only screen and (min-width: 992px) and (max-width: 1199px) {
+  .widget_list.widget_filter form input {
+    width: 65px;
+  }
+}
+.widget_list.widget_filter form button {
+  height: 30px;
+  line-height: 30px;
+  padding: 0 20px;
+  text-transform: capitalize;
+  color: #ffffff;
+  background: #333;
+  border: 0;
+  border-radius: 30px;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+}
+.widget_list.widget_filter form button:hover {
+  background: #DA627D;
+}
+
+.ui-slider-horizontal .ui-slider-range {
+  background: #DA627D;
+  height: 5px;
+}
+
+.ui-slider-horizontal {
+  height: 3px;
+  background: #dbdbdb;
+  border: none;
+  width: 92%;
+  margin: 0 auto;
+  margin-bottom: 22px;
+}
+
+.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default {
+  background: #fff;
+  border: 0;
+  border-radius: 0;
+  width: 19px;
+  height: 19px;
+  top: -7px;
+  cursor: pointer;
+  border-radius: 50%;
+  border: 5px solid #DA627D;
+}
 
       </style>
 </head>
@@ -245,7 +413,7 @@ a.sidebar-link:hover {
                 <div class="row">
                   
                     <div class="col-lg-3 col-md-12 col-sm-6 mb-4">
-                      <div class="wrapper">
+                      {{-- <div class="wrapper">
                           <ul class="sidebar-nav">
                               <li class="sidebar-item">
                                   <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#categories" aria-expanded="false" aria-controls="categories">
@@ -288,7 +456,137 @@ a.sidebar-link:hover {
                                   </ul>
                               </li>
                           </ul>
-                      </div>
+                      </div> --}}
+
+                      <div class="sidebar_widget">
+                        <div class="widget_inner">
+                            <div class="widget_list widget_filter sidebar-nav">
+                                <h2>Filter by price</h2>
+                                <form action="#">
+                                    <div id="slider-range"></div>
+                                    <button type="submit" style="margin-top: 10px;">Filter</button>
+                                    <input type="text" style="margin-top: 10px;" name="text" id="amount" />
+
+                                </form>
+                            </div>
+                            <div class="widget_list widget_categories sidebar-nav">
+                                <h2>Select By Categories</h2>
+                                <ul>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Men(6)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">women(10)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">kids(4)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Season(10)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Othercatagory(8)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+                            <div class="widget_list widget_categories sidebar-nav">
+                                <h2>Select By Brand</h2>
+                                <ul>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Prada(6)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Zara(10)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Celine(4)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Gucci(10)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Zara(8)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Dior(7)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Hema(6)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="widget_list widget_categories sidebar-nav">
+                                <h2>Select By Color</h2>
+                                <ul>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Black(6)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#"> Blue(8)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Brown(10)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#"> Green(6)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Pink(4)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">White(2)</a>
+                                        <span class="checkmark"></span>
+
+                                    </li>
+                                    <li>
+                                        <input type="checkbox">
+                                        <a href="#">Yellow(3)</a>
+                                        <span class="checkmark"></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="shop_sidebar_banner">
+                            <a href="#"><img src="assets/img/bg/banner9.jpg" alt=""></a>
+                        </div>
+                    </div>
                   </div>
                   
                   
@@ -302,12 +600,12 @@ a.sidebar-link:hover {
                                   <div class="product_container product-list-items">
                                       <div class="row">
                                           @foreach($products as $product)
-                                          <div class="col-sm-6 col-md-4 col-lg-3">
+                                          <div class="col-sm-6 col-md-4 col-lg-3" style=" padding-left: 0">
                                               <article class="single_product">
                                                   <figure>
                                                       <div class="product_thumb">
                                                           <a href="{{route('product-detail', $product->slug)}}">
-                                                            <img class="primary_img"
+                                                            <img class="primary_img" style="border-radius: 0.5rem 0.5rem 0 0;"
                                                             src="{{ $product->thumb_image }}" alt="{{ $product->name }}" class="img-fluid">
 
                                                           </a>
@@ -322,9 +620,9 @@ a.sidebar-link:hover {
                                                               </ul>
                                                           </div>
                                                       </div>
-                                                      <figcaption class="product_content text-center">
+                                                      <figcaption class="product_content text-center" style="border-radius: 0 0 0.5rem 0.5rem ;">
                                                           <div class="product_ratting">
-                                                              <div class="product-category float-left">{{ $product->subCategory->name ?? '' }}</div>
+                                                              <div class="product-category float-left">{{ $product->subCategory->name ?? $product->name }}</div>
                                                               {{-- <ul class="d-flex">
                                                                   <li><a href="#"><i class="ion-android-star"></i></a></li>
                                                                   <li><a href="#"><i class="ion-android-star"></i></a></li>
@@ -334,7 +632,7 @@ a.sidebar-link:hover {
                                                                   <li><span>(6)</span></li>
                                                               </ul> --}}
                                                           </div>
-                                                          <h4 class="product_name"><a href="{{route('product-detail', $product->slug)}}">{{ $product->name }}</a></h4>
+                                                          <h4 class="product_name overflow-hidden"><a href="{{route('product-detail', $product->slug)}}">{{ $product->name }}</a></h4>
                                                           <div class="price_box text-left">
                                                               <span class="text-black">₹{{$product->price}}</span>
                                                               <span class="old_price">₹{{$product->offer_price}}</span>
@@ -346,7 +644,7 @@ a.sidebar-link:hover {
                                                                   @csrf
                                                                   <input type="hidden" name="product_id" value="{{ $product->id }}">
                                                                   <!-- Quantity input -->
-                                                                  <button type="submit" class="btn btn-primary" data-tippy="Add To Cart" data-tippy-inertia="true" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-placement="top">Add To Cart</button>
+                                                                  <button type="submit" class="btn btn-primary te" data-tippy="Add To Cart" data-tippy-inertia="true" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-placement="top">Add To Cart</button>
                                                               </form>
                                                           </div>
                                                       @else
